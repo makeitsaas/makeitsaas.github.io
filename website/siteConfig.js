44fd2afe1f73fe01b4453f68d20daadce9bd2a7a
Duwab <contact@superpaas.pro>
2019-10-30 19:31:10 +0100

    Fix

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Make it SaaS - Methodology', // Title for your website.
   tagline: 'An efficient way to think large SaaS',
   url: 'https://makeitsaas.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/methodology/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
